6e60e4793ed6a5e5ad25e95e8e4b71f6d4788f47
Alex Kysorets <Alexkisorec@gmail.com>
2022-03-04 20:34:57 +0200

    + web api implementation for stats entity

---
+++ src/main/java/com/kysorets/kisakbets/service/stats/StatsService.java
@@ -0,0 +1,17 @@
+package com.kysorets.kisakbets.service.stats;
+
+import com.kysorets.kisakbets.model.Stats;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public interface StatsService {
+    Stats getStatsByName(String name);
+    Stats getStatsByType(String type);
+    Stats getStatsByStartedAndEndedTime(LocalDateTime startedAt, LocalDateTime endedAt);
+    List<Stats> getAllStats();
+    void saveStats(Stats stats);
+    void deleteStatsByName(String name);
+}
---
+++ src/main/java/com/kysorets/kisakbets/service/stats/StatsServiceImplementation.java
@@ -0,0 +1,45 @@
+package com.kysorets.kisakbets.service.stats;
+
+import com.kysorets.kisakbets.model.Stats;
+import com.kysorets.kisakbets.repository.StatsRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class StatsServiceImplementation implements StatsService{
+    private final StatsRepository statsRepository;
+
+    @Override
+    public Stats getStatsByName(String name) {
+        return statsRepository.getByName(name);
+    }
+
+    @Override
+    public Stats getStatsByType(String type) {
+        return statsRepository.getByType(type);
+    }
+
+    @Override
+    public Stats getStatsByStartedAndEndedTime(LocalDateTime startedAt, LocalDateTime endedAt) {
+        return statsRepository.getByStartedAtAndEndedAt(startedAt, endedAt);
+    }
+
+    @Override
+    public List<Stats> getAllStats() {
+        return statsRepository.findAll();
+    }
+
+    @Override
+    public void saveStats(Stats stats) {
+        statsRepository.save(stats);
+    }
+
+    @Override
+    public void deleteStatsByName(String name) {
+        statsRepository.deleteByName(name);
+    }
+}
