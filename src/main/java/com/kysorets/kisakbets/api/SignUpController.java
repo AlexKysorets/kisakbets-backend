9b6370b8bce96a8a132fe68f7f616e1294b20631
Alex Kysorets <Alexkisorec@gmail.com>
2022-03-10 15:23:11 +0200

    + successfull registration logic (auto-login after signup)

--- src/main/java/com/kysorets/kisakbets/api/SignUpController.java
+++ src/main/java/com/kysorets/kisakbets/api/SignUpController.java
@@ -3,6 +3,7 @@ package com.kysorets.kisakbets.api;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kysorets.kisakbets.model.Role;
 import com.kysorets.kisakbets.model.User;
+import com.kysorets.kisakbets.security.token.Token;
 import com.kysorets.kisakbets.service.role.RoleService;
 import com.kysorets.kisakbets.service.user.UserService;
 import lombok.Data;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.net.URI;
@@ -28,6 +30,7 @@ public class SignUpController {
     private final UserService userService;
     private final RoleService roleService;
     private final HttpServletResponse response;
+    private final HttpServletRequest request;
 
     @PostMapping("/signup")
     public void tryToSignUp(@RequestBody UserInfo userInfo) throws IOException {
@@ -35,7 +38,7 @@ public class SignUpController {
         if (user == null) {
             User user1 = userService.getUserByEmail(userInfo.getEmail());
             if (user1 == null) {
-                // successful registration
+                successfulRegistration(request, response, userInfo);
             } else {
                 unsuccessfulRegistration(response, "Email");
             }
@@ -44,11 +47,25 @@ public class SignUpController {
         }
     }
 
-    public void successfulRegistration(HttpServletResponse response, UserInfo userInfo) {
+    public void successfulRegistration(HttpServletRequest request, HttpServletResponse response, UserInfo userInfo) throws IOException {
         Role role = roleService.getRoleByName("ROLE_USER");
         User user = new User(userInfo.getUsername(), userInfo.getPassword(), userInfo.getEmail(), false,
                 new ArrayList<>(List.of(role)));
         userService.saveUser(user);
+
+        String access_token = new Token().giveAccessToken(user.getUsername(), request, user.getRoles(), null);
+        String refresh_token = new Token().giveRefreshToken(user.getUsername(), request);
+
+        Map<String, Object> result = new HashMap<>();
+        result.put("access_token", access_token);
+        result.put("refresh_token", refresh_token);
+        result.put("username", user.getUsername());
+        result.put("password", user.getPassword());
+        result.put("email", user.getEmail());
+        result.put("isVerified", user.isVerified());
+
+        response.setContentType(APPLICATION_JSON_VALUE);
+        new ObjectMapper().writeValue(response.getOutputStream(), result);
     }
 
     public void unsuccessfulRegistration(HttpServletResponse response, String type) throws IOException {
