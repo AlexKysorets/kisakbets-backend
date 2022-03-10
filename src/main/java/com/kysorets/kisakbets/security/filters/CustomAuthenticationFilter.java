1b3ecdec3a949ebe5b4aeeb2e9bc8bd60da161a1
Alex Kysorets <Alexkisorec@gmail.com>
2022-03-10 15:25:22 +0200

    * successful login user info

--- src/main/java/com/kysorets/kisakbets/security/filters/CustomAuthenticationFilter.java
+++ src/main/java/com/kysorets/kisakbets/security/filters/CustomAuthenticationFilter.java
@@ -63,12 +63,13 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
 
         // get tokens in json response
         com.kysorets.kisakbets.model.User userInfo = userService.getUserByUsername(user.getUsername());
-        Map<String, String> result = new HashMap<>();
+        Map<String, Object> result = new HashMap<>();
         result.put("username", userInfo.getUsername());
         result.put("password", userInfo.getPassword());
         result.put("email", userInfo.getEmail());
         result.put("access_token", access_token);
         result.put("refresh_token", refresh_token);
+        result.put("isVerified", userInfo.isVerified());
         response.setContentType(APPLICATION_JSON_VALUE);
         new ObjectMapper().writeValue(response.getOutputStream(), result);
     }
