b0009ef955e75ed73947097f50a57dda615a885b
Alex Kysorets <Alexkisorec@gmail.com>
2022-03-13 14:03:41 +0200

    + future redirect to frontend and change password mapping with algorithm

--- src/main/java/com/kysorets/kisakbets/api/ForgotPasswordController.java
+++ src/main/java/com/kysorets/kisakbets/api/ForgotPasswordController.java
@@ -11,10 +11,8 @@ import net.bytebuddy.utility.RandomString;
 import org.springframework.mail.MailSendException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
@@ -35,6 +33,7 @@ public class ForgotPasswordController {
     private final HttpServletResponse response;
     private final PasswordCodeService passwordCodeService;
     private final JavaMailSender javaMailSender;
+    private final PasswordEncoder passwordEncoder;
 
     @PostMapping("/send")
     public void forgotPassword(@RequestBody ForgotPassInfo info) throws IOException {
@@ -91,9 +90,49 @@ public class ForgotPasswordController {
             new ObjectMapper().writeValue(response.getOutputStream(), errors);
         }
     }
+
+    @GetMapping("/verify")
+    public void checkPasswordCode(@RequestParam("code") String code) throws IOException {
+        PasswordCode passwordCode = passwordCodeService.getPasswordCodeByCode(code);
+        LocalDateTime date = LocalDateTime.now().minusHours(24);
+        if (passwordCode.getExpiresAt().isAfter(date)) {
+            // redirect to frontend with requesting password verification code
+            // on frontend page user can create new password
+            Map<String, String> result = new HashMap<>();
+            result.put("message", "Redirect in future!");
+            response.setContentType(APPLICATION_JSON_VALUE);
+            new ObjectMapper().writeValue(response.getOutputStream(), result);
+        } else {
+            Map<String, String> errors = new HashMap<>();
+            response.setContentType(APPLICATION_JSON_VALUE);
+            response.setStatus(401);
+            errors.put("error", "Password code has expired!");
+            new ObjectMapper().writeValue(response.getOutputStream(), errors);
+        }
+    }
+
+    @PostMapping("/change")
+    public void changeUserPassword(@RequestBody ChangePasswordInfo info) throws IOException {
+        PasswordCode passwordCode = passwordCodeService.getPasswordCodeByCode(info.getPasswordCode());
+        User user = userService.getUserByEmail(passwordCode.getUser().getEmail());
+        user.setPassword(passwordEncoder.encode(info.getNewPassword()));
+        user.setPasswordCode("");
+        passwordCodeService.deletePasswordCodeByCode(info.getPasswordCode());
+        userService.saveUser(user);
+        Map<String, String> result = new HashMap<>();
+        result.put("message", "Password was successfully changed!");
+        response.setContentType(APPLICATION_JSON_VALUE);
+        new ObjectMapper().writeValue(response.getOutputStream(), result);
+    }
 }
 
 @Data
 class ForgotPassInfo {
     private String email;
+}
+
+@Data
+class ChangePasswordInfo {
+    private String newPassword;
+    private String passwordCode;
 }
