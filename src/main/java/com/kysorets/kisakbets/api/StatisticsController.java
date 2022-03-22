10790170929890ce275b890562a12eca6be4ebe1
Alex Kysorets <Alexkisorec@gmail.com>
2022-03-22 11:43:37 +0200

    + getting stats for previous month

--- src/main/java/com/kysorets/kisakbets/api/StatisticsController.java
+++ src/main/java/com/kysorets/kisakbets/api/StatisticsController.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kysorets.kisakbets.model.Stats;
 import com.kysorets.kisakbets.service.stats.StatsService;
 import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.jni.Local;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import java.time.LocalDateTime;
 import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -32,6 +34,22 @@ public class StatisticsController {
         String currentYear = String.valueOf(date.getYear());
         Stats statsSingle = statsService.getStatsByNameAndType(currentMonth + " " + currentYear, "single");
         Stats statsExpress = statsService.getStatsByNameAndType(currentMonth + " " + currentYear, "express");
+
+        List<Stats> stats = new ArrayList<>();
+        stats.add(statsSingle);
+        stats.add(statsExpress);
+        response.setContentType(APPLICATION_JSON_VALUE);
+        new ObjectMapper().writeValue(response.getOutputStream(), stats);
+    }
+
+    @GetMapping("/previous-month")
+    public void getStatsForPreviousMonth() throws IOException {
+        LocalDateTime date = LocalDateTime.now().minusMonths(1L);
+        String parseMonth = date.getMonth().toString().toLowerCase();
+        String previousMonth = parseMonth.substring(0, 1).toUpperCase() + parseMonth.substring(1);
+        String previousYear = String.valueOf(date.getYear());
+        Stats statsSingle = statsService.getStatsByNameAndType(previousMonth + " " + previousYear, "single");
+        Stats statsExpress = statsService.getStatsByNameAndType(previousMonth + " " + previousYear, "express");
 
         List<Stats> stats = new ArrayList<>();
         stats.add(statsSingle);
