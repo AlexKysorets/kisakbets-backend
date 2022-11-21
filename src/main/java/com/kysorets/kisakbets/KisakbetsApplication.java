9d38c249fc1740ff81344b50978567375573896b
Alex Kysorets <Alexkisorec@gmail.com>
2022-11-21 11:48:02 +0200

    * refactor cors configuration bean to separate java class

--- src/main/java/com/kysorets/kisakbets/KisakbetsApplication.java
+++ src/main/java/com/kysorets/kisakbets/KisakbetsApplication.java
@@ -2,27 +2,12 @@ package com.kysorets.kisakbets;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class KisakbetsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KisakbetsApplication.class, args);
-    }
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
-            }
-        };
     }
 
 }
